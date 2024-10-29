8bc3feadbe2f470b1a0c16d6e6c334fd146f3be1
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2024-10-29 02:46:26 +0000

    Create practice.php

---
+++ practice.php
@@ -0,0 +1,39 @@
+<?php
+echo 'hellp php!';
+// 問1
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+// 問2
+$array_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+echo $array_month[7];
+
+// 問3
+$hello = "Hello,";
+$name = "miwa";
+$world = "'s World";
+echo $hello . $name . $world;
+
+// 問4
+$tech_boost = "tech ";
+$tech_boost = "boost";
+echo "tech_boost";
+
+// 問5
+$_2018_calendar = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+echo $_2018_calendar["December"];
+
