65ab78a613c8e562be8fb6019624e2378230928f
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2024-10-30 02:39:12 +0000

    課題を修正しました

--- practice2.php
+++ practice2.php
@@ -2,10 +2,12 @@
 
 // 問1
 $name = "miwa";
-if ($name = "miwa") {
-    echo "私はmiwaです";
+if ($name == "miwa") {
+  echo "私はmiwaです";
+} else {
+  echo "あなたの名前ではありません";
 }
-
+echo "\n";
 // 問2
 $total = 0;
 echo $total;
@@ -13,19 +15,20 @@ for ($i = 0; $i <= 10000; $i++) {
   $total += $i;
 }
 echo $total;
-
+echo "\n";
 // 問3
 $fruits = array("apple", "strawberry", "grape", "banana", "orange");
 foreach($fruits as $fruit){
-    echo "". $fruit;
-    echo "\n";
+  echo $fruit;
+  echo "\n";
 }
-
+echo "\n";
 // 問4
 $start = 1;
 $end = 100;
 for($i = $start; $i <= $end; $i++) {
-    if($i % 5 == 0){
-        echo $i;
-      }
-    }
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
